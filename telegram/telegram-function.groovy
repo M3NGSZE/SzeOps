50ca22bf1fad47e68eaee24ab6aaaa9b1ece90c1
M3NGSZE <mengse.thun13@gmail.com>
2026-05-22 05:50:19 +0700

    function telegram

--- telegram/telegram-function.groovy
+++ telegram/telegram-function.groovy
@@ -5,11 +5,41 @@ pipeline {
         stage('Hello') {
             steps {
                 script{
-                def token="***********"
-                def chatId="**************"
-                sh """ curl -X POST https://api.telegram.org/bot${token}/sendMessage -d chat_id="${chatId}" -d "text=Hello from my terminal!" """
-            }
+                    // you need to repleace the value here {token} at def
+                    def token="token"
+                    def chatId="token"
+                    def message1="""
+                    hello world
+                    welcome to jenkins telelgram message 2
+                    """
+                    // sendTelegramMessage("${message1}", "${token}", "${chatId}")
+
+                    def message2="""
+                    *welcome to jenkins telelgram message*
+                    using second message
+                    """
+                    sendTelegramMessageV1("${message2}", "${token}", "${chatId}")
+                }
             }
         }
     }
+}
+
+def sendTelegramMessageV1(String message, String token, String chatId) {
+    sh """ 
+        curl -X POST https://api.telegram.org/bot${token}/sendMessage \
+        -d chat_id="${chatId}" -d parse_mode="Markdown" -d text="${message}" 
+    
+    """
+}
+
+def sendTelegramMessage(String message, String token, String chatId) {
+    // upgrade to use Markdown versin instead
+    def encodedMessage = URLEncoder.encode(message, "UTF-8")
+    sh """ 
+        curl -X POST https://api.telegram.org/bot${token}/sendMessage \\
+        -d chat_id="${chatId}" \\
+        -d text="${encodedMessage}" > /dev/null
+    
+    """
 }
