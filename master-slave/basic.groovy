3b130c4d866aeb5781f174423366f1c8f857c5cd
M3NGSZE <mengse.thun13@gmail.com>
2026-05-23 20:36:20 +0700

    testing slave and master

--- master-slave/basic.groovy
+++ master-slave/basic.groovy
@@ -1,10 +1,39 @@
 pipeline {
-    agent: any
+    
+    agent any
 
     stages {
-        step('Hello') {
+        stage('Work 1') {
+
+            agent {
+                label "slave-01"
+            }
+
             steps {
-                
+                sh """
+                    whoami
+
+                    curl ifconfig.me
+
+                    pwd
+                """
+            }
+        }
+
+        stage('Work 2') {
+
+            agent {
+                label "master"
+            }
+
+            steps {
+                sh """
+                    whoami
+
+                    curl ifconfig.me
+
+                    pwd
+                """
             }
         }
     }
