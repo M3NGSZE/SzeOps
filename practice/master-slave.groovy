06fc6ec2b4a798ad68a86ab0515daf7bcb372848
M3NGSZE <mengse.thun13@gmail.com>
2026-05-23 21:13:27 +0700

    half way

--- practice/master-slave.groovy
+++ practice/master-slave.groovy
@@ -1,7 +1,42 @@
 pipeline {
+    
     agent : any
+    
+    environment {
+        IMAGE_NAME="Jenkins-Master-Slave"
+        TAG="${env.BUILD_NUMBER}"
+    }
 
     stages {
-        
+        // clone code from github
+        stage('Clone code') {
+            steps {
+                git 'https://github.com/M3NGSZE/reactjs-devop11-template.git'
+            }
+        }
+
+        // scan code with sonarqube
+        stage('Scan code') {
+            environment {
+                scannerHome= tool 'sonar-scanner' 
+            }
+
+            steps {
+                withSonarQubeEnv(credentialsId: 'SONARQUBE_TOKEN', installationName: 'sonar-scanner') {
+                script{
+                    def projectKey = 'reactjs-devops11-template' 
+                    def projectName = 'ReactjsDevOps11template'
+                    def projectVersion = '1.0.0'
+                    
+                    sh """
+                        ${scannerHome}/bin/sonar-scanner \
+                        -Dsonar.projectKey=${projectKey} \
+                        -Dsonar.projectName="${projectName}" \
+                        -Dsonar.projectVersion=${projectVersion} \
+                    """   
+                    }
+                }
+            }
+        }
     }
 }
