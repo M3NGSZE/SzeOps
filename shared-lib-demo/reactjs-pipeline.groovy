2ec7d7fe82d1798d9755140474fe07e079a7b4eb
M3NGSZE <mengse.thun13@gmail.com>
2026-05-27 00:36:55 +0700

    sharelib

--- shared-lib-demo/reactjs-pipeline.groovy
+++ shared-lib-demo/reactjs-pipeline.groovy
@@ -1,13 +1,92 @@
 // if you are using the shared library, you must specified it  here
-@library([""])
+@library(["jenkins-shared-lib@master"]) _
 
 pipeline {
+
+    // any agent available to run it , run it 
     agent any
-
+    environment{
+        IMAGE_NAME="jenkins-reactjs-img"
+        TAG="${env.BUILD_NUMBER}" // built-in env 
+    }
     stages {
-        stage('Hello') {
+        stage('Clone Code ') {
             steps {
+                git 'https://github.com/M3NGSZE/reactjs-devop11-template.git'
+            }
+        }
 
+        stage("Check Code Quality in Sonarqube "){
+            
+            environment {
+                scannerHome= tool 'sonar-scanner' 
+            }
+
+            steps{
+                withSonarQubeEnv(credentialsId: 'SONARQUBE_TOKEN', installationName: 'sonar-scanner') {
+                script{
+                    def projectKey = 'reactjs-devops11-template' 
+                    def projectName = 'ReactjsDevOps11template'
+                    def projectVersion = '1.0.0' 
+                    
+                    scanSonarqube("${projectKey}", "${projectName}", "${projectVersion}")
+                    }
+                }
+            }
+        }
+
+        // Check the quality gate ( passed or failed )
+        stage("Wait for Quality Gate "){
+            steps{
+                script{
+                    // We must configure webhook to let jenkins know when the result is return 
+                    def qg = waitForQualityGate()
+                    if ( qg.status != 'OK'){
+                        sh """
+                            echo " No need to build since you QG is failed "
+                        """
+                        currentBuild.result='FAILURE'
+                        error("Quality Gate is Failed !! ")
+                        return 
+                    }else {
+                        echo "Quality of code is okay!! "
+                        currentBuild.result='SUCCESS'
+                    }
+                }
+            }
+        }
+
+        stage('Building Image') {
+            steps {
+                sh """
+                docker build -t reactjs-demo-image  . 
+                """
+            }
+        }
+
+        //  Push the docker image to the dockerhub 
+        stage("Push Image to Dockerhub "){
+            steps{
+                withCredentials([usernamePassword(credentialsId: 'DOCKERHUB-CRED', passwordVariable: 'TOKEN', usernameVariable: 'USERNAME')]) {
+
+                    sh """
+                        echo "1. Login to Dockerhub account " 
+                        echo "$TOKEN" | docker login -u ${USERNAME} --password-stdin
+
+                        docker tag reactjs-demo-image ${USERNAME}/${IMAGE_NAME}:v1.0.${TAG}
+                        echo "2. Push image to Dockerhub"
+                        docker push ${USERNAME}/${IMAGE_NAME}:v1.0.${TAG}
+                    """
+                }
+            }
+        }
+
+        post{
+            always{
+                def message = "Hello world from share lib jenkins"
+                def token = "8723150702:AAHwv5FHEwwqAuLGnLCtnXCdNPasoGYbK_w"
+                def chatId = "934577494"
+                telegramMessage("${message}", "${token}", "${chatId}")
             }
         }
     }
